6ed319084a98451b59f623d2157e61571cf81e1c
Laiamdelprado <lmarco@userlytics.com>
2022-12-23 17:23:31 +0100

    feat(): filter completed/pending/all applied

---
+++ todo-app/src/app/todos/filter.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { filterTypes } from '../filter/filter.actions';
+import { Todo } from './models/todo.model';
+
+@Pipe({
+  name: 'filterTodo',
+})
+export class FilterPipe implements PipeTransform {
+  transform(todos: Todo[], filter: filterTypes): Todo[] {
+    switch (filter) {
+      case 'completed':
+        return todos.filter((todo) => todo.completed);
+      case 'pending':
+        return todos.filter((todo) => !todo.completed);
+      default:
+        return todos;
+    }
+  }
+}
--- todo-app/src/app/todos/todo-list/todo-list.component.ts
+++ todo-app/src/app/todos/todo-list/todo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/app.reducer';
+import { filterTypes } from 'src/app/filter/filter.actions';
 import { Todo } from '../models/todo.model';
 
 @Component({
@@ -10,10 +11,14 @@ import { Todo } from '../models/todo.model';
 })
 export class TodoListComponent implements OnInit {
   todos: Todo[] = [];
+  currentFilter!: filterTypes;
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
-    this.store.select('todos').subscribe((todos) => (this.todos = todos));
+    this.store.subscribe(({todos, filter}) => {
+      this.todos = todos;
+      this.currentFilter = filter;
+    });
   }
 }
