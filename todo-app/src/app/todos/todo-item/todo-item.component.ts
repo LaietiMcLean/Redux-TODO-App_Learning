1b8915a8b315ebbfe7285f58aa29a334311de3c5
Laiamdelprado <lmarco@userlytics.com>
2022-12-23 12:48:29 +0100

    feat(): mark as complete or unmark actions

--- todo-app/src/app/todos/todo-item/todo-item.component.ts
+++ todo-app/src/app/todos/todo-item/todo-item.component.ts
@@ -1,15 +1,16 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Todo } from '../models/todo.model';
 import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/app.reducer';
+import * as actions from '../todo.actions'
 
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
-  styleUrls: ['./todo-item.component.css']
+  styleUrls: ['./todo-item.component.css'],
 })
 export class TodoItemComponent implements OnInit {
-
-
   @Input()
   todo!: Todo;
   @ViewChild('inputText') txtInputHTML!: ElementRef;
@@ -18,22 +19,24 @@ export class TodoItemComponent implements OnInit {
   txtInput!: FormControl;
   editing: boolean = false;
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.checkCompleted = new FormControl(this.todo.completed);
     this.txtInput = new FormControl(this.todo.text, Validators.required);
+    this.checkCompleted.valueChanges.subscribe((value) => {
+      this.store.dispatch(actions.toggle({id: this.todo.id}))
+    });
   }
 
   edit() {
     this.editing = true;
     setTimeout(() => {
       this.txtInputHTML.nativeElement.select();
-    }, 1)
+    }, 1);
   }
 
   editingEnd() {
     this.editing = false;
   }
-
 }
--- todo-app/src/app/todos/todo.reducer.ts
+++ todo-app/src/app/todos/todo.reducer.ts
@@ -1,15 +1,27 @@
-import { createReducer, on } from "@ngrx/store";
-import { Todo } from "./models/todo.model";
-import { create } from "./todo.actions";
+import { createReducer, on } from '@ngrx/store';
+import { Todo } from './models/todo.model';
+import { create, toggle } from './todo.actions';
 
 export const initialState: Todo[] = [
   new Todo('Laundry'),
   new Todo('Lunch'),
   new Todo('Clean the house'),
-  new Todo('Feed the cats')
+  new Todo('Feed the cats'),
 ];
 
 export const todosReducer = createReducer(
   initialState,
-  on(create, (state, {text}) => [...state, new Todo(text)])
+  on(create, (state, { text }) => [...state, new Todo(text)]),
+  on(toggle, (state, { id }) => {
+    return state.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          completed: !todo.completed,
+        };
+      } else {
+        return todo;
+      }
+    });
+  })
 );
